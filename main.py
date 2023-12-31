c7b76c8df6fe4f82bc5a5f4a068053ee5fb49d24
AnKun10 <114561027+AnKun10@users.noreply.github.com>
2023-12-31 18:30:13 +0700

    edit

--- main.py
+++ main.py
@@ -4,21 +4,35 @@ from colordetection import ColorDetector
 from copy import deepcopy
 
 
+def detect_new_state():
+    detector = ColorDetector()
+    detector.color_detecting()
+    return detector.state
+
+
+def show_detector_notes():
+    print("=============================== RUBIK COLOR DETECTOR ===============================")
+    print("1, PRESS 't' TO CAPTURE A SIDE FACELETS' COLOR.")
+    print("2, PRESS 's' TO SAVE THAT SIDE FACELETS' COLOR.")
+    print("3, PRESS 'q' TO END DETECTING PROCESS.")
+    print("====================================================================================")
+
+
 def show_options():
     print("=============================== RUBIK CUBE SOLVER ===============================")
-    print("1, LAYER BY LAYER.")
-    print("2, BFS & BB.")
-    print("3, KOCIEMBA.")
-    print("4, KORF.")
+    print("1, COLOR DETECTING NEW RUBIK CUBE.")
+    print("2, LAYER BY LAYER.")
+    print("3, BFS & BB.")
+    print("4, KOCIEMBA.")
+    print("5, KORF.")
     print("0, QUIT.")
     print("=================================================================================")
 
 
 def main():
-    detector = ColorDetector()
-    detector.color_detecting()
+    show_detector_notes()
 
-    state = detector.state
+    state = detect_new_state()
     cube = RubikCube(state=state)
     cube.show()
     solution = []
@@ -31,20 +45,24 @@ def main():
         except ValueError:
             print("INVALID INPUT! PLEASE TRY AGAIN!")
         if solver_choice == 1:
+            state = detect_new_state()
+            cube = RubikCube(state=state)
+            cube.show()
+        elif solver_choice == 2:
             temp_cube = deepcopy(cube)
             solver = LayerByLayer(temp_cube)
             solution = solver.solve()
             print("SOLUTION: ", end="")
             for move in solution:
                 print(move, end=" ")
-        elif solver_choice == 2:
+        elif solver_choice == 3:
             temp_cube = BFSBBCube(state=state)
             solver = BFSBB(cube=cube)
             solution = solver.solve()
             print("SOLUTION: ", end="")
             for move in solution:
                 print(move, end=" ")
-        elif solver_choice == 3:
+        elif solver_choice == 4:
             solver = Kociemba(state=state)
             solver.solve()
             break
