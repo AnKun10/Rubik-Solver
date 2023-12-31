b526ee06ae86f7dfdb29d30648914f6481c6e2e3
AnKun10 <114561027+AnKun10@users.noreply.github.com>
2023-12-31 19:47:19 +0700

    edit

--- gen_scramble.py
+++ gen_scramble.py
@@ -2,16 +2,16 @@ from rubikcube import RubikCube
 
 cube = RubikCube()
 
-while True:
-    rot_nums = []
-    try:
-        rot_nums = [int(x) for x in input("Number of rotations = ").split()]
-    except ValueError:
-        print("Invalid Input! Please Try again!")
-        continue
-    break
+# while True:
+#     rot_nums = []
+#     try:
+#         rot_nums = [int(x) for x in input("Number of rotations = ").split()]
+#     except ValueError:
+#         print("Invalid Input! Please Try again!")
+#         continue
+#     break
 
-# rot_nums = [3, 4, 5, 7, 10, 20, 50, 80, 100]
+rot_nums = [3, 4, 5, 7, 10, 20, 50, 80, 100]
 f = open("test_set.txt", "w")
 for rot_num in rot_nums:
     cube.shuffle(min_rot=rot_num, max_rot=rot_num)
